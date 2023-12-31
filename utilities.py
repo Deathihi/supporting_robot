a0482771e9308b20981b77cef7c9db678608a4c4
Deathihi <92397342+Deathihi@users.noreply.github.com>
2023-12-31 16:36:59 +0700

    utilities.py

--- utilities.py
+++ utilities.py
@@ -1,72 +1,99 @@
 import cv2
 import numpy as np
-
-# Global variables for perspective transformation
-src_points = np.float32([(200, 720), (1100, 720), (590, 450), (690, 450)])
-dst_points = np.float32([(300, 720), (980, 720), (300, 0), (980, 0)])
-
-# Function to apply perspective transform
-def warp_image(img):
-    M = cv2.getPerspectiveTransform(src_points, dst_points)
-    warped_img = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]), flags=cv2.INTER_LINEAR)
-    return warped_img
-
-# Callback function for track bars
-def update_perspective(val):
-    global src_points, dst_points
-    src_points[0, 0] = cv2.getTrackbarPos('Top-Left X', 'Perspective Adjustment')
-    src_points[0, 1] = cv2.getTrackbarPos('Top-Left Y', 'Perspective Adjustment')
-    src_points[1, 0] = cv2.getTrackbarPos('Top-Right X', 'Perspective Adjustment')
-    src_points[1, 1] = cv2.getTrackbarPos('Top-Right Y', 'Perspective Adjustment')
-    src_points[2, 0] = cv2.getTrackbarPos('Bottom-Left X', 'Perspective Adjustment')
-    src_points[2, 1] = cv2.getTrackbarPos('Bottom-Left Y', 'Perspective Adjustment')
-    src_points[3, 0] = cv2.getTrackbarPos('Bottom-Right X', 'Perspective Adjustment')
-    src_points[3, 1] = cv2.getTrackbarPos('Bottom-Right Y', 'Perspective Adjustment')
-
-# Function to set up track bars for perspective adjustment
-def setup_trackbars():
-    cv2.createTrackbar('Top-Left X', 'Perspective Adjustment', src_points[0, 0], 1920, update_perspective)
-    cv2.createTrackbar('Top-Left Y', 'Perspective Adjustment', src_points[0, 1], 1080, update_perspective)
-    cv2.createTrackbar('Top-Right X', 'Perspective Adjustment', src_points[1, 0], 1920, update_perspective)
-    cv2.createTrackbar('Top-Right Y', 'Perspective Adjustment', src_points[1, 1], 1080, update_perspective)
-    cv2.createTrackbar('Bottom-Left X', 'Perspective Adjustment', src_points[2, 0], 1920, update_perspective)
-    cv2.createTrackbar('Bottom-Left Y', 'Perspective Adjustment', src_points[2, 1], 1080, update_perspective)
-    cv2.createTrackbar('Bottom-Right X', 'Perspective Adjustment', src_points[3, 0], 1920, update_perspective)
-    cv2.createTrackbar('Bottom-Right Y', 'Perspective Adjustment', src_points[3, 1], 1080, update_perspective)
-
-# Capture video from the camera
-cap = cv2.VideoCapture(0)
-
-# Set up track bars
-cv2.namedWindow('Perspective Adjustment')
-setup_trackbars()
-
-while True:
-    # Read a frame from the camera
-    ret, frame = cap.read()
-
-    # Crop the region of interest
-    roi = frame[450:720, 200:1100]
-
-    # Apply color detection (you can replace this with your color detection code)
-    # For example, you can use cv2.inRange to filter a specific color range
-    lower_white = np.array([200, 200, 200], dtype=np.uint8)
-    upper_white = np.array([255, 255, 255], dtype=np.uint8)
-    mask_white = cv2.inRange(roi, lower_white, upper_white)
-    result = cv2.bitwise_and(roi, roi, mask=mask_white)
-
-    # Apply perspective transform
-    bird_eye_view = warp_image(result)
-
-    # Display the frame, region of interest, and bird's eye view
-    cv2.imshow('Original Frame', frame)
-    cv2.imshow('Region of Interest', roi)
-    cv2.imshow('Bird Eye View', bird_eye_view)
-
-    # Break the loop if 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release the camera and close all windows
-cap.release()
-cv2.destroyAllWindows()
+ 
+def thresholding(img):
+    imgHsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    lowerWhite = np.array([80,0,0])
+    upperWhite = np.array([255,160,255])
+    maskWhite = cv2.inRange(imgHsv,lowerWhite,upperWhite)
+    return maskWhite
+ 
+def warpImg(img,points,w,h,inv = False):
+    pts1 = np.float32(points)
+    pts2 = np.float32([[0,0],[w,0],[0,h],[w,h]])
+    if inv:
+        matrix = cv2.getPerspectiveTransform(pts2, pts1)
+    else:
+        matrix = cv2.getPerspectiveTransform(pts1,pts2)
+    imgWarp = cv2.warpPerspective(img,matrix,(w,h))
+    return imgWarp
+ 
+def nothing(a):
+    pass
+ 
+def initializeTrackbars(intialTracbarVals,wT=480, hT=240):
+    cv2.namedWindow("Trackbars")
+    cv2.resizeWindow("Trackbars", 360, 240)
+    cv2.createTrackbar("Width Top", "Trackbars", intialTracbarVals[0],wT//2, nothing)
+    cv2.createTrackbar("Height Top", "Trackbars", intialTracbarVals[1], hT, nothing)
+    cv2.createTrackbar("Width Bottom", "Trackbars", intialTracbarVals[2],wT//2, nothing)
+    cv2.createTrackbar("Height Bottom", "Trackbars", intialTracbarVals[3], hT, nothing)
+ 
+def valTrackbars(wT=480, hT=240):
+    widthTop = cv2.getTrackbarPos("Width Top", "Trackbars")
+    heightTop = cv2.getTrackbarPos("Height Top", "Trackbars")
+    widthBottom = cv2.getTrackbarPos("Width Bottom", "Trackbars")
+    heightBottom = cv2.getTrackbarPos("Height Bottom", "Trackbars")
+    points = np.float32([(widthTop, heightTop), (wT-widthTop, heightTop),
+                      (widthBottom , heightBottom ), (wT-widthBottom, heightBottom)])
+    return points
+ 
+def drawPoints(img,points):
+    for x in range(4):
+        cv2.circle(img,(int(points[x][0]),int(points[x][1])),15,(0,0,255),cv2.FILLED)
+    return img
+ 
+def getHistogram(img,minPer=0.1,display= False,region=1):
+ 
+    if region ==1:
+        histValues = np.sum(img, axis=0)
+    else:
+        histValues = np.sum(img[img.shape[0]//region:,:], axis=0)
+ 
+    #print(histValues)
+    maxValue = np.max(histValues)
+    minValue = minPer*maxValue
+ 
+    indexArray = np.where(histValues >= minValue)
+    basePoint = int(np.average(indexArray))
+    #print(basePoint)
+ 
+    if display:
+        imgHist = np.zeros((img.shape[0],img.shape[1],3),np.uint8)
+        for x,intensity in enumerate(histValues):
+            cv2.line(imgHist,(x,img.shape[0]),(x,img.shape[0]-int(intensity//255//region)),(255,0,255),1)
+            cv2.circle(imgHist,(basePoint,img.shape[0]),20,(0,255,255),cv2.FILLED)
+        return basePoint,imgHist
+ 
+    return basePoint
+ 
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
